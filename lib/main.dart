c59e21e4ae44677898999175ca214416d57884bf
Muhammed Safuvan <muhammedsafuvan024@gmail.com>
2020-12-16 20:31:39 +0530

    card added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
       home: QuoteList(),
@@ -11,22 +12,53 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<String> quotes = [
-    'The best among you is the one who doesn’t harm others with his tongue and hands.',
-    'The best way to get started is to quit talking and begin doing.',
-    'The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.',
+  List<Quote> quotes = [
+    Quote(author: 'Prophet Muhammad (PBUH)',text:'The best among you is the one who doesn’t harm others with his tongue and hands.' ),
+    Quote(author: 'Walt Disney',text:'The best way to get started is to quit talking and begin doing.' ),
+    Quote(author: 'Winston Churchill',text: 'The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.'),
+
   ];
+
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 6.0,),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Awesome quotes'),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.indigo[800],
+        backgroundColor: Colors.indigo,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
         ),
     );
   }
